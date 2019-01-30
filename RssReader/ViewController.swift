49afe476317d5a370b155cf2a5e22a81c2e6bee8
Andrew Borisov <shadowreal85@gmail.com>
2019-01-30 14:18:45 +0300

    Initial Commit

---
+++ RssReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by Andrew Borisov on 1/30/19.
+//  Copyright © 2019 Andrew Borisov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
