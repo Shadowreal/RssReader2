cc5cf6f36d971569e6db8aeebde39b359fdef050
Andrew Borisov <andrewborisov@MacBook-Air-Andrew.local>
2019-02-03 10:34:55 +0300

    aaa

---
+++ RssReader/Structures/StructName.swift
@@ -0,0 +1,15 @@
+//
+//  StructName.swift
+//  RssReader
+//
+//  Created by Andrew Borisov on 2/3/19.
+//  Copyright © 2019 Andrew Borisov. All rights reserved.
+//
+
+import Foundation
+
+
+struct Post {
+    var title: String?
+    var 
+}
